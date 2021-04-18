debce60a69c03c160dd0929bf48da684421c46b1
Fennix <n303p4@users.noreply.gitlab.com>
2021-04-17 21:51:16 -0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,4 +1,4 @@
 if status --is-interactive
-    foxfetch -c -g -w
+    foxfetch --disable cpu --disable gpu -w
     echo -s " Type " (tput bold) help (tput sgr0) " for instructions on how to use fish"
 end
