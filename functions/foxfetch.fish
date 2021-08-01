4bfe76c2caa16f6c1ea16b694d83b41cb3d9a955
Fennix <n303p4@users.noreply.gitlab.com>
2021-08-01 02:52:50 -0400

    always use printf instead of checking version

--- functions/foxfetch.fish
+++ functions/foxfetch.fish
@@ -59,14 +59,8 @@ function foxfetch_mem_usage_linux
             set mem_used (math $mem_used-(foxfetch_kib_value $keyvaluepair[2]))
         end
     end
-    set -l fish_version (fish --version | cut -f1 -d . | cut -f3 -d " ")
-    if [ $fish_version -le 2 ]
-        echo -s (printf "%.0f" (math "$mem_used/1024")) " MiB / " \
-                (printf "%.0f" (math "$mem_total/1024")) " MiB"
-    else
-        echo -s (math "round($mem_used/1024)") " MiB / " \
-                (math "round($mem_total/1024)") " MiB"
-    end
+    echo -s (printf "%.0f" (math "$mem_used/1024")) " MiB / " \
+            (printf "%.0f" (math "$mem_total/1024")) " MiB"
 end
 
 
