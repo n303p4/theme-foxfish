6c97481609b1dbc14de4c3e34b678f8653b8b51f
Fennix <n303p4@noreply.gitlab.users.com>
2021-08-14 01:02:34 -0400

    Don't run _git_branch_name twice

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -60,12 +60,14 @@ function fish_prompt
     echo -n -s $bg_blue " $cwd " $normal
 
     # Show git branch and dirty state
-    if [ (command -v git > /dev/null; and _git_branch_name) ]
+    if [ (command -v git > /dev/null) ]
         set -l git_branch (_git_branch_name)
-        if [ (_is_git_dirty) ]
-            echo -n -s $bg_cyan $black " git:$git_branch " $red "$__foxfish_glyph_flag " $normal
-        else
-            echo -n -s $bg_cyan $black " git:$git_branch " $normal
+        if test -n $git_branch
+            if [ (_is_git_dirty) ]
+                echo -n -s $bg_cyan $black " git:$git_branch " $red "$__foxfish_glyph_flag " $normal
+            else
+                echo -n -s $bg_cyan $black " git:$git_branch " $normal
+            end
         end
     end
 
