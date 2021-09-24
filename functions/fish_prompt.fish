3b893b00729426962c9a5be2b46a3b11b13c862e
Fennix <n303p4@noreply.gitlab.users.com>
2021-09-24 16:53:17 -0400

    use $hostname

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -45,7 +45,7 @@ function fish_prompt
 
     # Display whether I am SSHed or not
     if test -n "$SSH_TTY"
-        echo -n -s $bg_white $magenta " ssh:" (whoami) @ (hostname) " " $normal
+        echo -n -s $bg_white $magenta " ssh:" (whoami) @ $hostname " " $normal
     end
 
     # Display current time
